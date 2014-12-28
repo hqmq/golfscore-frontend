30bbe24de444a132583e6ceb8a0abe3fc81eafe4
hqmq <michael@riesd.com>
2014-12-28 11:55:19 -0700

    simplify build process

--- app/js/assets.js
+++ app/js/assets.js
@@ -1,9 +1,9 @@
 module.exports = {
   urlFor: function(path){
-    if (process.env.NODE_ENV === "development") {
-      return "//localhost:8080"+path;
-    } else {
+    if (process.env.NODE_ENV === "production") {
       return "//example.com"+path;
+    } else {
+      return "//localhost:8080"+path;
     }
   }
 }
