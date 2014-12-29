e71f3a5b923e0e080b677b89b61d4736dd9c1f34
hqmq <michael@riesd.com>
2014-12-29 15:43:29 -0700

    got GoogleLineChart working with static data

--- app/js/components/game.js
+++ app/js/components/game.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM **/
 var React = require('react');
 var moment = require('moment');
+var GoogleLineChart = require('./google_line_chart');
 var ScoreTable = require('./score_table');
 var xhr = require('xhr');
 
@@ -11,6 +12,7 @@ var Game = React.createClass({
       course_id: null,
       course: null,
       played_at: null,
+      player_averages: [],
       scores: [],
     };
   },
@@ -26,10 +28,12 @@ var Game = React.createClass({
 
   render(){
     var mom = moment.unix(this.state.played_at);
+    var data = [["blank","jared cook","Mikey","russell","nate priego","ny","chris hopkins","par"],[1,0,-1,2,1,0,2,0],[2,-1,-1,2,1,0,3,0],[3,-2,-1,1,1,-1,4,0],[4,-2,0,0,1,0,3,0],[5,-3,0,1,2,1,2,0],[6,-3,0,0,2,2,1,0],[7,-3,1,0,2,2,1,0],[8,-3,1,0,2,2,1,0],[9,-2,-1,-1,3,2,0,0],[10,-2,-1,0,3,2,3,1],[11,-2,-1,0,3,2,3,2],[12,-1,-1,0,3,1,3,2],[13,-1,-3,0,1,1,3,1],[14,-3,-4,1,-1,2,5,1],[15,-4,-4,2,0,2,7,2],[16,-4,-4,1,1,3,6,2],[17,-5,-4,1,2,3,5,2],[18,-5,-4,1,2,3,5,2]];
     return (
       <div className="container-vertical">
         <h2>{this.state.course} :: {mom.format("MMM D, YYYY @ h:mm a")}</h2>
         <ScoreTable scores={this.state.scores} />
+        <GoogleLineChart data={data} height="500px" width="1100px" graphId="running_totals" />
       </div>
     );
   }
---
+++ app/js/components/google_line_chart.js
@@ -0,0 +1,34 @@
+/** @jsx React.DOM **/
+var React = require('react');
+
+/**
+Assumes that the Google Visualizations "CoreChart" package is already loaded
+So in your <head> you will need:
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript">¬
+    google.load('visualization','1',{packages:['corechart']});¬
+  </script>¬
+**/
+var GoogleLineChart = React.createClass({
+  componentDidMount() {
+    this.drawCharts();
+  },
+  componentDidUpdate() {
+    this.drawCharts();
+  },
+  render() {
+    return <div id={this.props.graphId} style={{height: this.props.height, width: this.props.width}}></div>;
+  },
+
+  drawCharts(){
+    var data = google.visualization.arrayToDataTable(this.props.data);
+    var options = {
+      title: 'Running Totals vs Average'
+    };
+    var node = document.getElementById(this.props.graphId);
+    var chart = new google.visualization.LineChart(node);
+    chart.draw(data, options);
+  }
+});
+
+module.exports = GoogleLineChart;
