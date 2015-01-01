c7be33b1656c6259d0a725f9dbbb18d15fe2de25
hqmq <michael@riesd.com>
2015-01-01 09:02:35 -0700

    replace the hostname during dev builds

--- app/js/assets.js
+++ app/js/assets.js
@@ -1,7 +1,7 @@
 module.exports = {
   urlFor: function(path){
     if (process.env.NODE_ENV === "production") {
-      return "//example.com"+path;
+      return "//golf.riesd.com"+path;
     } else {
       return "//localhost:8080"+path;
     }
